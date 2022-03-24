61197564f476c53080847c6558614be649de291e
LEEYOONJONG <yoonjong1820@khu.ac.kr>
2022-03-24 18:08:21 +0900

    Add exception handling

--- nowmusic/LoginViewController.swift
+++ nowmusic/LoginViewController.swift
@@ -12,38 +12,68 @@ class LoginViewController: UIViewController {
    
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.loginButton.isEnabled = false
+        self.configureInputField()
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func loginButtonTapped(_ sender: Any) {
-        if idTextField.text != "" && passwordTextField.text != "" && ageTextField.text != "" {
+        if idTextField.text != "" && passwordTextField.text != ""  {
             guard let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as? MainViewController else { return }
-            mainViewController.id = idTextField.text
-            mainViewController.password = passwordTextField.text
+            mainViewController.id = idTextField.text!
+            mainViewController.password = passwordTextField.text!
+            print("보내는 ID, PW : \(mainViewController.id), \(mainViewController.password)")
             sendString()
-            guard let mainTabBarController = self.storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") else { return }
-            self.navigationController?.pushViewController(mainTabBarController, animated: true)
             
         }
     }
     
     func sendString(){
-        // guard let url = URL(string: "http://192.168.0.11:8080/db/user/members/login?userId=yoonjong&password=1234")
-        if let url = URL(string: "http://192.168.0.11:8080/db/user/members/login?userId=yoonjong&password=1234"){
+        // guard let url = URL(string: "http://192.168.0.11:8080/db/user/memberrs/login?userId=yoonjong&password=1234")
+        if let url = URL(string: "http://192.168.0.11:8080/db/user/members/login?userId=\(idTextField.text!)&password=\(passwordTextField.text!)"){
             var request = URLRequest.init(url: url)
             request.httpMethod = "GET"
             URLSession.shared.dataTask(with: request) { (data, response, error) in
                 guard let data = data else { return }
                 guard let response = response else { return }
-                print("data : \(data)")
+//                print("data : \(data)")
                 print("response: \(response)")
-                
+                if let httpResponse = response as? HTTPURLResponse{
+                    let httpStatusCode = httpResponse.statusCode
+                    if httpStatusCode == 200 {
+                        DispatchQueue.main.sync{
+                            guard let mainTabBarController = self.storyboard?.instantiateViewController(withIdentifier: "MainTabBarController") else { return }
+                            self.navigationController?.pushViewController(mainTabBarController, animated: true)
+                        }
+                    }
+                    else {
+                        print("유저 정보가 없습니다")
+                    }
+                    
+                }
             }.resume()
+        }
+    }
+    private func configureInputField(){
+        self.passwordTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        self.idTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        
+    }
+}
+extension LoginViewController{
+    @objc func textFieldDidChange(_ sender:Any?){
+        if self.idTextField.text != "" && self.passwordTextField.text != "" {
+            self.loginButton.isEnabled = true
+        }
+        else {
+            self.loginButton.isEnabled = false
         }
     }
 }
--- nowmusic/SignupViewController.swift
+++ nowmusic/SignupViewController.swift
@@ -8,28 +8,36 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-
+    
     @IBOutlet weak var idTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var ageTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var genreTextField: UITextField!
     
+    @IBOutlet weak var completeButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.completeButton.isEnabled = false
+        self.configureInputField()
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
-
+    
     @IBAction func signupButtonTapped(_ sender: UIButton) {
         if idTextField.text != "" && passwordTextField.text != "" && ageTextField.text != "" && nameTextField.text != "" && genreTextField.text != "" {
             
             signupLogic()
-            self.navigationController?.popViewController(animated: true)
+            
         }
         
     }
+    
+    
     private func signupLogic(){
         print(idTextField.text!, passwordTextField.text!, nameTextField.text!, ageTextField.text!, genreTextField.text!)
         let dic:Dictionary = ["userId" : idTextField.text!, "password" : passwordTextField.text!,"name" : nameTextField.text!, "age" :  ageTextField.text!, "favoriteGenre" : genreTextField.text!]
@@ -51,8 +59,62 @@ class SignupViewController: UIViewController {
         print("URLSession 진입")
         let session = URLSession.shared
         session.dataTask(with: request, completionHandler: {(data, response, error) in
-            print("전송완료")
+            print("회원가입 전송완료")
+            if let httpResponse = response as? HTTPURLResponse{
+                let httpStatusCode = httpResponse.statusCode
+                let successRange = (200..<300)
+                print("--> statuscode: \(httpStatusCode)")
+                if successRange.contains(httpStatusCode) {
+                    DispatchQueue.main.async{
+                        let alert = UIAlertController(title: "회원가입이 성공했습니다.", message: "", preferredStyle: .alert)
+                        self.present(alert, animated: true, completion: nil)
+                        let when = DispatchTime.now() + 3
+                        DispatchQueue.main.asyncAfter(deadline: when){
+                            // your code with delay
+                            alert.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                    
+                    print("새로운 아이디 등록이 성공했습니다.")
+                    self.navigationController?.popViewController(animated: true)
+                }
+                else {
+                    DispatchQueue.main.async{
+                        let alert = UIAlertController(title: "이미 등록된 아이디입니다.", message: "", preferredStyle: .alert)
+                        self.present(alert, animated: true, completion: nil)
+                        let when = DispatchTime.now() + 3
+                        DispatchQueue.main.asyncAfter(deadline: when){
+                            // your code with delay
+                            alert.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                    print("이미 등록된 아이디입니다.")
+                    self.idTextField.text = ""
+                    self.idTextField.placeholder = "다시 입력해주세요."
+                }
+                
+            }
         }).resume()
     }
-
+    private func configureInputField(){
+        self.idTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        self.passwordTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        self.ageTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        self.nameTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        self.genreTextField.addTarget(self, action: #selector(self.textFieldDidChange(_:)), for: .editingChanged)
+        
+    }
+}
+extension SignupViewController{
+    @objc func textFieldDidChange(_ sender:Any?){
+        if self.idTextField.text != "" && self.passwordTextField.text != ""
+            && self.ageTextField.text != ""  && self.nameTextField.text != ""
+            && self.genreTextField.text != ""
+        {
+            self.completeButton.isEnabled = true
+        }
+        else {
+            self.completeButton.isEnabled = false
+        }
+    }
 }
